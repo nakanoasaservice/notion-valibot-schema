442b65359745b52722249a4cf46dc10204cc7733
Nakano as a Service <nakanoasaservice@gmail.com>
2025-12-22 19:56:05 +0900

    refactor: brushup

--- src/test-utils.ts
+++ src/test-utils.ts
@@ -1,8 +1,6 @@
 import type { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
-export type Extends<T, U> = T extends U ? true : false;
-
-export type NotionProperty = PageObjectResponse["properties"][string];
+type NotionProperty = PageObjectResponse["properties"][string];
 
 export type NonNullableValues<T> = { [K in keyof T]: NonNullable<T[K]> };
 
