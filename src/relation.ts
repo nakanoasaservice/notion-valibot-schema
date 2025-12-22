91b90c28048ad26b6cc97f3ab06c2bb7ea72d5fe
Nakano as a Service <nakanoasaservice@gmail.com>
2025-12-23 02:56:11 +0900

    feat: refactor RelationSchema to use InnerRelationSchema

--- src/relation.ts
+++ src/relation.ts
@@ -1,30 +1,24 @@
 import * as v from "valibot";
 
+const InnerRelationSchema = v.object({ id: v.string() });
+
 export const RelationSchema = v.pipe(
 	v.object({
-		relation: v.array(v.object({ id: v.string() })),
+		relation: v.array(InnerRelationSchema),
 	}),
 	v.transform((v) => v.relation.map((r) => r.id)),
 );
 
 export const SingleRelationSchema = v.pipe(
 	v.object({
-		relation: v.tuple([
-			v.object({
-				id: v.string(),
-			}),
-		]),
+		relation: v.tuple([InnerRelationSchema]),
 	}),
 	v.transform((v) => v.relation[0].id),
 );
 
 export const NullableSingleRelationSchema = v.pipe(
 	v.object({
-		relation: v.array(
-			v.object({
-				id: v.string(),
-			}),
-		),
+		relation: v.array(InnerRelationSchema),
 	}),
 	v.transform((v) => v.relation[0]?.id ?? null),
 );
