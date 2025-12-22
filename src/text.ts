6f944103130d974ba6ac7d8acc59114924aa1fba
Nakano as a Service <nakanoasaservice@gmail.com>
2025-12-23 03:04:44 +0900

    feat: refactor RichText schemas to simplify transformation logic

--- src/text.ts
+++ src/text.ts
@@ -1,24 +1,21 @@
 import * as v from "valibot";
 
-const RichTextArraySchema = v.pipe(
-	v.array(
-		v.object({
-			plain_text: v.string(),
-		}),
-	),
-	v.transform((v) => v.map((v) => v.plain_text).join("")),
+const RichTextArraySchema = v.array(
+	v.object({
+		plain_text: v.string(),
+	}),
 );
 
 export const TitleSchema = v.pipe(
 	v.object({
 		title: RichTextArraySchema,
 	}),
-	v.transform((v) => v.title),
+	v.transform((v) => v.title.map((v) => v.plain_text).join("")),
 );
 
 export const RichTextSchema = v.pipe(
 	v.object({
 		rich_text: RichTextArraySchema,
 	}),
-	v.transform((v) => v.rich_text),
+	v.transform((v) => v.rich_text.map((v) => v.plain_text).join("")),
 );
